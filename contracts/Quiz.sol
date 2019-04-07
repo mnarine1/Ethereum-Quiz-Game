a45cda99c6efaf720aacf46a967593728e414fa0
mnarine1 <38436299+mnarine1@users.noreply.github.com>
2019-04-07 15:48:28 -0400

    Add files via upload

---
+++ contracts/Quiz.sol
@@ -0,0 +1,54 @@
+pragma solidity ^5.0.0
+
+contract Quiz {
+
+   struct Question {
+      string question;
+      string correctAns;
+      string wrongAns1;
+      string wrongAns2;
+      string wrongAns3;
+   }
+
+   struct QuizEvent {
+      uint id;
+      string name;
+      uint pool;
+      Question q1;
+      mapping(address => bool) attempts;
+   }
+
+   mapping(uint => QuizEvent) public quizzes;
+
+   uint public numQuizzes;
+
+   event fetchquiz (uint indexed _quizId);   // get quiz questions
+   event quiztaken (uint indexed _quizId);   // quiz answers submitted
+
+   constructor () public {
+      makeQuiz("Test Quiz 1", 10, "2+2=", "4", "3", "5", "2");
+   }
+
+   function makeQuiz (string memory _name, uint memory _pool, string memory _question, string memory _ans1, string memory _ans2, string memory _ans3, string memory _ans4 private {
+      numQuizzes ++;
+      quizzes[numQuizzes] = Quiz(numQuizzes, _name, _pool, Question(_question, _ans1, _ans2, _ans3, _ans4));
+   }
+
+   function getQuiz (uint _quizId) public {
+      require(!quizzes[_quizId].attempts[msg.sender]);
+      require(_quizId > 0 && _quizId < = numQuizzes);
+      quizzes[_quizId].attempts[msg.sender] = true;
+      emit fetchquiz(_quizId);
+   }
+
+   function scoreAttempt (uint _quizId, string _ans) returns (bool) {
+      require(!quizzes[_quizId].attempts[msg.sender]);
+      require(_quizId > 0 && _quizId < = numQuizzes);
+
+      if(quizzes[_quizId].q1.correctAns == _ans) {
+         return true;
+      } else {
+         return false;
+      }
+   }
+}
