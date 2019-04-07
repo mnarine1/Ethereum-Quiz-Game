b6635b662d677ebbe17d37fb30586cc6292f7210
mnarine1 <38436299+mnarine1@users.noreply.github.com>
2019-04-07 17:10:45 -0400

    Add files via upload

--- contracts/Quiz.sol
+++ contracts/Quiz.sol
@@ -1,4 +1,4 @@
-pragma solidity ^5.0.0
+pragma solidity ^0.5.0;
 
 contract Quiz {
 
@@ -18,7 +18,7 @@ contract Quiz {
       mapping(address => bool) attempts;
    }
 
-   mapping(uint => QuizEvent) public quizzes;
+   mapping(uint => QuizEvent) quizzes;
 
    uint public numQuizzes;
 
@@ -29,23 +29,23 @@ contract Quiz {
       makeQuiz("Test Quiz 1", 10, "2+2=", "4", "3", "5", "2");
    }
 
-   function makeQuiz (string memory _name, uint memory _pool, string memory _question, string memory _ans1, string memory _ans2, string memory _ans3, string memory _ans4 private {
+   function makeQuiz (string memory _name, uint _pool, string memory _question, string memory _ans1, string memory _ans2, string memory _ans3, string memory _ans4) private {
       numQuizzes ++;
-      quizzes[numQuizzes] = Quiz(numQuizzes, _name, _pool, Question(_question, _ans1, _ans2, _ans3, _ans4));
+      quizzes[numQuizzes] = QuizEvent(numQuizzes, _name, _pool, Question(_question, _ans1, _ans2, _ans3, _ans4));
    }
 
    function getQuiz (uint _quizId) public {
       require(!quizzes[_quizId].attempts[msg.sender]);
-      require(_quizId > 0 && _quizId < = numQuizzes);
+      require(_quizId > 0 && _quizId <= numQuizzes);
       quizzes[_quizId].attempts[msg.sender] = true;
       emit fetchquiz(_quizId);
    }
 
-   function scoreAttempt (uint _quizId, string _ans) returns (bool) {
+   function scoreAttempt (uint _quizId, string memory _ans) public returns (bool) {
       require(!quizzes[_quizId].attempts[msg.sender]);
-      require(_quizId > 0 && _quizId < = numQuizzes);
+      require(_quizId > 0 && _quizId <= numQuizzes);
 
-      if(quizzes[_quizId].q1.correctAns == _ans) {
+      if(keccak256(abi.encodePacked(quizzes[_quizId].q1.correctAns)) == keccak256(abi.encodePacked(_ans))) {
          return true;
       } else {
          return false;
